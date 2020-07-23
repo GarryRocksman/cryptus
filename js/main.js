ec9f1e68f7b70d0e503095465df1623d0b6794d5
Igor Bondarenko <garryrocksmanjs@gmail.com>
2020-07-23 19:35:36 +0300

    bug fixed

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 window.onload = function () {
     var openButton = document.getElementById('open-button');
     var closeButton = document.getElementById('close-button');
+    var navigationItems = document.getElementsByClassName('nav-item');
     
 
     openButton.onclick = function () {
@@ -17,6 +18,17 @@ window.onload = function () {
             behavior: 'smooth'
         });
     };
+
+    for (var i = 0; i < navigationItems.length; i++) {
+        navigationItems.item(i).onclick = function () {
+            openButton.style.display = 'block';
+            window.scroll({
+                top: 0, 
+                left: 0, 
+                behavior: 'smooth'
+            });
+        };
+    }
 
     
 };
